3768a183e4b4644aa20e67bca333a83bae9237e7
coding_babo <kwondevelop@gmail.com>
2026-05-21 20:01:17 +0900

    260520 [develop]

--- src/main/resources/schema.v000/260519-02_create-posts.sql
+++ src/main/resources/schema.v000/260519-02_create-posts.sql
@@ -2,7 +2,7 @@ CREATE TABLE `posts` (
     `id`         BIGINT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
     `user_id`    BIGINT UNSIGNED             NOT NULL,
     `content`    VARCHAR(255)                NOT NULL,
-    `image`      VARCHAR(20)                 NOT NULL,
+    `image`      VARCHAR(100)                NOT NULL,
     `created_at` DATETIME                    NOT NULL DEFAULT CURRENT_TIMESTAMP(),
     `updated_at` DATETIME                    NOT NULL DEFAULT CURRENT_TIMESTAMP(),
     `deleted_at` DATETIME                             DEFAULT NULL
