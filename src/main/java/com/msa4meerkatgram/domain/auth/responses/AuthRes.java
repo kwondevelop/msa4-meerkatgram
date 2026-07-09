ce41bfc25e377beeea28e50b28f645cbefe5a78d
coding_babo <kwondevelop@gmail.com>
2026-07-09 18:09:26 +0900

    add Springdoc OpenApi

--- src/main/java/com/msa4meerkatgram/domain/auth/responses/AuthRes.java
+++ src/main/java/com/msa4meerkatgram/domain/auth/responses/AuthRes.java
@@ -3,9 +3,11 @@ package com.msa4meerkatgram.domain.auth.responses;
 import com.msa4meerkatgram.domain.user.entities.User;
 import com.msa4meerkatgram.domain.user.responses.UserRes;
 import com.msa4meerkatgram.domain.user.responses.UserWithPostCountRes;
+import com.msa4meerkatgram.global.annotations.openapi.ApiNotValidErrorResponse;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
-@Builder
+@Schema(description = "로그인 리스폰스")
 public record AuthRes(
     UserWithPostCountRes user
     ,String accessToken
---
+++ src/main/java/com/msa4meerkatgram/global/responses/GlobalRes.java
@@ -0,0 +1,11 @@
+package com.msa4meerkatgram.global.responses;
+
+public record GlobalRes<T>(
+    String code,
+    String message,
+    T data
+)   {
+    public static <T> GlobalRes<T> from(String code, String message, T data) {
+        return new GlobalRes<T>(code, message, data);
+    }
+}
